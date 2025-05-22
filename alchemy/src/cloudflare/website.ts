feddac63b169da111e63a8055d0f4b18ee057ccf
Sam Goodwin <sam@aspect.sh>
2025-05-22 00:46:09 -0700

    chore(release): 0.16.10

--- alchemy/src/cloudflare/website.ts
+++ alchemy/src/cloudflare/website.ts
@@ -61,7 +61,7 @@ export type Website<B extends Bindings> = B extends { ASSETS: any }
 
 export async function Website<B extends Bindings>(
   id: string,
-  props: WebsiteProps<B>
+  props: WebsiteProps<B>,
 ): Promise<Website<B>> {
   if (props.bindings?.ASSETS) {
     throw new Error("ASSETS binding is reserved for internal use");
@@ -99,8 +99,8 @@ export async function Website<B extends Bindings>(
               compatibility_flags: props.compatibilityFlags ?? [],
             },
             null,
-            2
-          )
+            2,
+          ),
         );
       }
     }
