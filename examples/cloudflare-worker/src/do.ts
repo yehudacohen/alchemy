6f25111436cfa4afa366554173bf89d2b504ec2d
Sam Goodwin <sam@alchemy.run>
2025-05-28 00:07:22 -0700

    chore: enhance cloudflare-worker RPC example

--- examples/cloudflare-worker/src/do.ts
+++ examples/cloudflare-worker/src/do.ts
@@ -4,26 +4,21 @@ import { DurableObject } from "cloudflare:workers";
  * A simple Hello World Durable Object
  */
 export class HelloWorldDO extends DurableObject {
-  constructor(
-    private readonly state: DurableObjectState,
-    env: CloudflareEnv,
-  ) {
-    super(state, env);
-  }
+  async increment() {
+    // Get the current count from storage or initialize to 0
+    let count = (await this.ctx.storage.get("count")) || 0;
 
-  async hello() {
-    return "Hello World";
+    // Store the updated count
+    await this.ctx.storage.put("count", count);
+
+    return count;
   }
 
   /**
    * Handle HTTP requests to the Durable Object
    */
   async fetch(_request: Request): Promise<Response> {
-    // Get the current count from storage or initialize to 0
-    let count = (await this.state.storage.get("count")) || 0;
-
-    // Store the updated count
-    await this.state.storage.put("count", count);
+    const count = await this.increment();
 
     // Return a response with the count
     return new Response(
--- examples/cloudflare-worker/src/worker.ts
+++ examples/cloudflare-worker/src/worker.ts
@@ -1,17 +1,17 @@
+import { env } from "cloudflare:workers";
 import type { queue, worker } from "../alchemy.run.js";
 export * from "./do.js";
 export * from "./workflow.js";
 
 export default {
-  async fetch(_request: Request, env: typeof worker.Env) {
+  async fetch(_request: Request) {
     await env.QUEUE.send({
       name: "John Doe",
       email: "john.doe@example.com",
     });
 
     const obj = env.DO.get(env.DO.idFromName("foo"));
-    await obj.hello();
-    await obj.hello();
+    await obj.increment();
     async function _foo() {
       // @ts-expect-error - foo doesn't exist on the HelloWorldDO class
       await obj.foo();
