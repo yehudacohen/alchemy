2f720c575fe6cf9c0b683ecf9b6f04fea29d1f00
Sam Goodwin <sam@alchemy.run>
2025-06-01 13:40:36 -0700

    chore: add NPM_TOKEN

--- stacks/repo.run.ts
+++ stacks/repo.run.ts
@@ -104,6 +104,7 @@ await Promise.all([
     SENTRY_ORG: await alchemy.secret.env.SENTRY_ORG,
     VERCEL_ACCESS_TOKEN: await alchemy.secret.env.VERCEL_ACCESS_TOKEN,
     ALCHEMY_PASSWORD: await alchemy.secret.env.ALCHEMY_PASSWORD,
+    NPM_TOKEN: await alchemy.secret.env.NPM_TOKEN,
   }).flatMap(async ([name, value]) => {
     const props = {
       owner: "sam-goodwin",
