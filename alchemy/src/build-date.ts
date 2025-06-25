b953e51de5443704f3ce11648e085f056d346be0
Sam Goodwin <sam@alchemy.run>
2025-06-25 00:36:40 -0700

    chore: fix the build

--- alchemy/src/build-date.ts
+++ alchemy/src/build-date.ts
@@ -5,4 +5,4 @@
  * The build date used as the default worker compatibility date.
  * This is set to the date when the package was built.
  */
-export const BUILD_DATE = "2025-06-23";
+export const BUILD_DATE = "2025-06-25";
