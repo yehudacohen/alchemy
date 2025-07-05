d41ebae7ec6b59de1b9189564aa2c77a2a6e506b
Sam Goodwin <sam@alchemy.run>
2025-07-04 22:16:25 -0700

    fix: remove debug logs from create CLI

--- alchemy/bin/services/package-manager.ts
+++ alchemy/bin/services/package-manager.ts
@@ -83,9 +83,6 @@ export async function installDependencies(
   const commands = getPackageManagerCommands(pm);
 
   try {
-    console.log(await fs.readJson(`${targetCwd}/package.json`));
-    console.log({ dependencies });
-    console.log({ devDependencies });
     if (!dependencies && !devDependencies) {
       await execa(commands.install, {
         cwd: targetCwd,
